dfaa7464809e2ad712293c235c04f94e01927916
rainerw <rainerw@github.com>
2012-01-03 14:55:05 +0100

    sun import

--- sonferenz-db/src/main/java/de/bitnoise/sonferenz/model/ActionModel.java
+++ sonferenz-db/src/main/java/de/bitnoise/sonferenz/model/ActionModel.java
@@ -3,25 +3,19 @@ package de.bitnoise.sonferenz.model;
 import java.util.Date;
 
 import javax.persistence.Column;
-import javax.persistence.Embeddable;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import lombok.Data;
+
 import org.hibernate.annotations.Type;
 
 import com.sun.istack.internal.Nullable;
-
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 @Entity
 @Data
